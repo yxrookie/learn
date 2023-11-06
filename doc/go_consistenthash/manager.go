3464a220d4b67d18ac42393fc4f3f4b46a6554ae
yxrookie <lyxrookie@qq.com>
2023-11-06 19:22:14 +0800

    一致性哈希算法的三个类别实现

---
+++ doc/go_consistenthash/manager.go
@@ -0,0 +1,134 @@
+package go_consistenthash
+
+import (
+	"sync"
+	hashring "go_consistenthash/hashring"
+	
+)
+
+type NodeManager[T any] struct {
+	mux      sync.RWMutex
+	nodes    map[string]Node[T]
+	hashRing *hashring.HashRing[T]
+	replicas int
+}
+
+func NewManager[T any](replicas int) *NodeManager[T] {
+	return &NodeManager[T]{
+		nodes:    make(map[string]Node[T]),
+		hashRing: hashring.New[T](),
+		replicas: replicas,
+	}
+}
+
+// Add node to manager
+func (m *NodeManager[T]) Add(node *Node[T]) {
+	m.mux.Lock()
+	defer m.mux.Unlock()
+
+	m.nodes[node.key] = *node
+	m.hashRing.Add(node.Virtuals(m.replicas)...)
+}
+
+// GetNode returns manager node
+func (m *NodeManager[T]) GetNode(key string) (*Node[T], bool) {
+	m.mux.RLock()
+	defer m.mux.RUnlock()
+
+	node, ok := m.nodes[key]
+	if !ok {
+		return nil, false
+	}
+	return &node, true
+}
+
+// GetNodes returns total manager nodes
+func (m *NodeManager[T]) GetNodes() []Node[T] {
+	m.mux.RLock()
+	defer m.mux.RUnlock()
+
+	var list = make([]Node[T], 0, len(m.nodes))
+	for _, node := range m.nodes {
+		list = append(list, node)
+	}
+	return list
+}
+
+// Remove node by key
+func (m *NodeManager[T]) Remove(nodeKey string) {
+	m.mux.Lock()
+	defer m.mux.Unlock()
+
+	node, ok := m.nodes[nodeKey]
+	if !ok {
+		return
+	}
+
+	delete(m.nodes, nodeKey)
+	m.hashRing.Remove(node.VirtualKeys(m.replicas)...)
+}
+
+// FindOne returns hashring slot by key
+func (m *NodeManager[T]) FindOne(key string) (hashring.Slot[T], bool) {
+	m.mux.RLock()
+	defer m.mux.RUnlock()
+
+	return m.hashRing.Get(key)
+}
+
+func (m *NodeManager[T]) find(key string, num int,
+	fn func(hashring.Slot[T]) hashring.Slot[T]) []hashring.Slot[T] {
+	m.mux.RLock()
+	defer m.mux.RUnlock()
+
+	var slots = make([]hashring.Slot[T], 0, num)
+	slot, ok := m.hashRing.Get(key)
+	if !ok {
+		return slots
+	}
+	slots = append(slots, slot)
+
+	// cache slot
+	cache := map[uint32]bool{
+		slot.Hash(): true,
+	}
+
+	var next = fn(slot)
+	for i := 1; i < num; i++ {
+		if cache[next.Hash()] {
+			return slots
+		}
+
+		slots = append(slots, next)
+		cache[next.Hash()] = true
+
+		next = fn(next)
+	}
+	return slots
+}
+
+// FindNext returns hashring slots by key and next num
+func (m *NodeManager[T]) FindNext(key string, num int) []hashring.Slot[T] {
+	return m.find(key, num, m.hashRing.GetNext)
+}
+
+// FindPrev returns hashring slots by key and prev num
+func (m *NodeManager[T]) FindPrev(key string, num int) []hashring.Slot[T] {
+	return m.find(key, num, m.hashRing.GetPrev)
+}
+
+// Count returns manager nodes count
+func (m *NodeManager[T]) Count() int {
+	m.mux.RLock()
+	defer m.mux.RUnlock()
+
+	return len(m.nodes)
+}
+
+// Slots returns hashring slots count
+func (m *NodeManager[T]) Slots() int {
+	m.mux.RLock()
+	defer m.mux.RUnlock()
+
+	return m.hashRing.Count()
+}
---
+++ doc/go_consistenthash/node.go
@@ -0,0 +1,65 @@
+package go_consistenthash
+
+import (
+	"strconv"
+    hashring "go_consistenthash/hashring"
+	
+)
+
+type Node[T any] struct {
+	Data   T
+	key    string
+	weight int
+}
+
+func NewNode[T any](key string, data T) *Node[T] {
+	return &Node[T]{
+		Data:   data,
+		key:    key,
+		weight: 1,
+	}
+}
+
+func (n *Node[T]) virtualKey(index int) string {
+	return n.key + "#" + strconv.FormatInt(int64(index), 10)
+}
+
+// Virtuals returns virtual nodes
+func (n *Node[T]) Virtuals(replicas int) []hashring.Slot[T] {
+	var (
+		total = n.weight * replicas
+		slots = make([]hashring.Slot[T], 0, total)
+	)
+	for i := 0; i < total; i++ {
+		key := n.virtualKey(i)
+		slots = append(slots, hashring.NewSlot(key, n.Data))
+	}
+	return slots
+}
+
+// VirtualKeys returns virtual keys
+func (n *Node[T]) VirtualKeys(replicas int) []string {
+	var (
+		total = n.weight * replicas
+		keys  = make([]string, 0, total)
+	)
+	for i := 0; i < total; i++ {
+		keys = append(keys, n.virtualKey(i))
+	}
+	return keys
+}
+
+// SetWeight
+func (n *Node[T]) SetWeight(w int) {
+	n.weight = w
+}
+
+// GetWeight
+func (n *Node[T]) GetWeight() int {
+	return n.weight
+}
+
+// GetKey
+func (n *Node[T]) GetKey() string {
+	return n.key
+}
