ff5ca27146c2cca6f8f26cc926dc127244d7e8f7
rookieyx <lyxrookie@qq.com>
2023-11-19 17:16:44 +0800

    变式 4-查找最后一个值小于或等于给定值的元素

---
+++ doc/go_datastructure/bsearch/bsearchVariant4_test.go
@@ -0,0 +1,13 @@
+package bsearch
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBsearchVariant4(t *testing.T) {
+	nums := []int{0, 0, 0, 0, 1, 1, 1, 23, 34, 546, 546, 546, 789}
+	fmt.Println(bsearchVariant4(nums, 0 ,len(nums)-1, 0))
+	fmt.Println(bsearchVariant4(nums, 0 ,len(nums)-1, 1))
+	fmt.Println(bsearchVariant4(nums, 0 ,len(nums)-1, 546))
+}
